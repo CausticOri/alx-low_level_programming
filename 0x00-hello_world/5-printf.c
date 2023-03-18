e35e50f79ae0776148778e39017138b9ec2f969f
Caustic <stephen.boakye@zenpetroleum.com>
2023-03-18 06:47:49 -0700

    My first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - 5-printf.c
  * description -Write a C program that prints exactly with proper grammar
@@ -8,6 +8,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 )
