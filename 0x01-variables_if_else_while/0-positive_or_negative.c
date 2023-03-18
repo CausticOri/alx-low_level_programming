f2c22d33b0e2012591ac20a342e5e4dc947d5437
Caustic <stephen.boakye@zenpetroleum.com>
2023-03-18 08:51:04 -0700

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,8 @@
 
 #include <stdio.h>
 
-
 /**
  * main - print if number is positive, zero or negative
- *
  * Return: Always  (Success)
  *
 */
@@ -15,20 +13,21 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
-	printf("%d is positive\n",n);
-        }
-        else if (n == 0)
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
 	{
-	printf("%d is zero\n",n);
-        }
-        else
+	printf("%d is zero\n", n);
+	}
+	else
 	{
-	printf("%d is negative\n",n);
-        }
-        return (0);
-}	
+	printf("%d is negative\n", n);
+	}
+	return (0);
+}
