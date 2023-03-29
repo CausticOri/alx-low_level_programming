845f2410fe8fdde2e57c4423adc4b454c17b4db8
Caustic <stephen.boakye@zenpetroleum.com>
2023-03-29 07:06:11 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * puts - prints a string, followed by a new line, to stdout
- * @str: string to print
+ * _puts - prints a string
+ * @str: the string to print
+ * Return: void
  */
-void_puts(char *str)
+void _puts(char *str)
 {
-	while (*str != '\n')
+	for (; *str != '\0'; str++)
 	{
-		_putchar(*str++);
+	_putchar(*str);
 	}
 	_putchar('\n');
 }
