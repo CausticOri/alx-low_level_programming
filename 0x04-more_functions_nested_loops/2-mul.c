c46301a303985472064ba493e992bc3dfbb874ac
Caustic <stephen.boakye@zenpetroleum.com>
2023-03-31 15:54:16 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul  - multiplies two integers
+ * @a: the first number to multiple
+ * @b: the second number to multiply with the first number
+ * Return: the result of the multiplication between a param and b param
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
