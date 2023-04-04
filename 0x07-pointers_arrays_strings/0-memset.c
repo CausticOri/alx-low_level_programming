936335a4c5204c7e180823041f701a5b8c06b033
Caustic <stephen.boakye@zenpetroleum.com>
2023-04-04 03:30:50 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * _memset - entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: always 0 (success)
+ */
+
+void *_memset(char *s,char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[1] = b;
+	return (s);
+}
